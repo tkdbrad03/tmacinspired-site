1fb75cb636cfe4c7d382ee304588972d15280765
tkdbrad03 <info@tmacmastermind.com>
2026-03-22 04:18:19 -0400

    Add files via upload

---
+++ tax-sale-academy-sw.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = 'tax-sale-academy-v1';
+var urlsToCache = [
+  '/tax-sale-academy',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;1,400&family=DM+Sans:wght@300;400;500;600&display=swap'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).catch(function() {
+        return caches.match('/tax-sale-academy');
+      });
+    })
+  );
+});
